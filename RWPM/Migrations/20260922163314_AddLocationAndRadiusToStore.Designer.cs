86697ed8a0c2e92841e035671f0491bd112ff4ce
ducdzvcl2004 <ducnahe180290@fpt.edu.vn>
2026-09-23 20:56:22 +0700

    them migration

--- RWPM/Migrations/20260922163314_AddLocationAndRadiusToStore.Designer.cs
+++ RWPM/Migrations/20260922163314_AddLocationAndRadiusToStore.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RWPM.Infrastructure.Data;
 
@@ -11,9 +12,10 @@ using RWPM.Infrastructure.Data;
 namespace RWPM.Migrations
 {
     [DbContext(typeof(DefaultDatabaseContext))]
-    partial class DefaultDatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20260922163314_AddLocationAndRadiusToStore")]
+    partial class AddLocationAndRadiusToStore
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
